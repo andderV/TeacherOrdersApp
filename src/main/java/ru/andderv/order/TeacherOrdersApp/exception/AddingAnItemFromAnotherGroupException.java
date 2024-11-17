2b2487df9aada875489c6ae9d7da434f5190c7be
Panacea <dergus@yahoo.com>
2024-11-17 18:56:48 +0300

    версия 2.0.1 начал разработку нового функционала (поставщики-контракты-заказы-остатки-преподаватели) добавил новые классы: Contracts, ContractsGroceries, GroceriesMeasureUnit, GroupOfMU, OrdersToSuppliers, OrdersToSuppliersGrocery, Providers, TempOrders. Также добавил к каждому классу слой репозиторий, слой сервиса и контроллеры. Плюс ко всем новым классам добавил views. У единиц измерения есть коэффициент, который указывает на то, является ли ед.изм. базовой для своей группы. К товару можно доавлять ед.измерения только из одной группы. На карточке товара и при вычислении остатка, будет использоваться базовая единица. Если у определенного товара есть другие ед.изм, кроме базовой, то они будут использоваться для расчетов внутри контракта.

---
+++ src/main/java/ru/andderv/order/TeacherOrdersApp/exception/AddingAnItemFromAnotherGroupException.java
@@ -0,0 +1,12 @@
+package ru.andderv.order.TeacherOrdersApp.exception;
+
+/**
+ * @author andderV
+ * @version 16.11.2024 17:34
+ * TeacherOrdersApp
+ */
+public class AddingAnItemFromAnotherGroupException extends RuntimeException {
+    public AddingAnItemFromAnotherGroupException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ru/andderv/order/TeacherOrdersApp/repositories/GroceriesRepository.java
+++ src/main/java/ru/andderv/order/TeacherOrdersApp/repositories/GroceriesRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.andderv.order.TeacherOrdersApp.models.Groceries;
+import ru.andderv.order.TeacherOrdersApp.models.GroceriesMeasureUnit;
 import ru.andderv.order.TeacherOrdersApp.models.MeasureUnit;
 
 import java.util.Optional;
